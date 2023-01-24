540d22ed00f547037b7c4406b9838f87bb8b408c
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-24 21:05:37 +0200

    Update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Stack, Text } from '@chakra-ui/react';
+import { Button, Link, Stack, Text } from '@chakra-ui/react';
 import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 
@@ -18,22 +18,45 @@ const Root: React.FC = () => {
 	if (status === 'loading') return <LoadingPage />;
 	if (status === 'unauthenticated') {
 		return (
-			<Page wrapperProps={{ background: '#FFF8F8' }}>
-				<Layout max="md">
-					<Stack direction="column" alignItems="center">
-						<Image src={logo} alt="JShare" width="300" />
-
+			<Page
+				wrapperProps={{ background: '#FFF8F8' }}
+				footer={
+					<Layout noMargin max="md" p="4">
 						<Button
 							mt="4"
 							colorScheme="green"
 							variant="solid"
 							onClick={() => signIn()}
-							maxWidth="300px"
-							width="100%"
+							width="full"
 							alignSelf="center"
 						>
 							Sign in
 						</Button>
+					</Layout>
+				}
+			>
+				<Layout max="md">
+					<Stack direction="column" alignItems="center">
+						<Image src={logo} alt="JShare" width="300" />
+						<Text textAlign="center">
+							Welcome!
+							<br />
+							<br />
+							JShare is an{' '}
+							<Link
+								href="https://github.com/juiceo/jshare"
+								color="teal.500"
+							>
+								open source hobby project
+							</Link>{' '}
+							to try and replace the void left by the discontinued
+							WeShare app. Please note that JShare is still in a
+							very early stage of development, so things may break
+							and/or change on you unexpectedly.
+							<br />
+							<br />
+							Hope you enjoy it!
+						</Text>
 					</Stack>
 				</Layout>
 			</Page>
