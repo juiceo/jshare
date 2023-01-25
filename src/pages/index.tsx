fde14ea96492be79d645fd35bd177b1e29971f7a
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-25 08:48:33 +0200

    Modify landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,25 +38,6 @@ const Root: React.FC = () => {
 				<Layout max="md">
 					<Stack direction="column" alignItems="center">
 						<Image src={logo} alt="JShare" width="300" />
-						<Text textAlign="center">
-							Welcome!
-							<br />
-							<br />
-							JShare is an{' '}
-							<Link
-								href="https://github.com/juiceo/jshare"
-								color="teal.500"
-							>
-								open source hobby project
-							</Link>{' '}
-							to try and replace the void left by the discontinued
-							WeShare app. Please note that JShare is still in a
-							very early stage of development, so things may break
-							and/or change on you unexpectedly.
-							<br />
-							<br />
-							Hope you enjoy it!
-						</Text>
 					</Stack>
 				</Layout>
 			</Page>
