72b64f15c4f40362f0cfe54edf46d8e23fa89531
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-25 08:44:02 +0200

    Add Facebook login method

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import { AppProviders } from 'next-auth/providers';
+import FacebookProvider from 'next-auth/providers/facebook';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -11,60 +12,80 @@ const {
 	GOOGLE_OAUTH_CLIENT_SECRET,
 	GITHUB_OAUTH_CLIENT_ID,
 	GITHUB_OAUTH_CLIENT_SECRET,
+	FACEBOOK_CLIENT_ID,
+	FACEBOOK_CLIENT_SECRET,
 } = process.env;
 
 const providers: AppProviders = [];
 
-if (!GOOGLE_OAUTH_CLIENT_ID || !GOOGLE_OAUTH_CLIENT_SECRET) {
-	throw new Error(
-		'GOOGLE_OAUTH_CLIENT_ID and GOOGLE_OAUTH_CLIENT_SECRET must be set',
+if (!!GOOGLE_OAUTH_CLIENT_ID && !!GOOGLE_OAUTH_CLIENT_SECRET) {
+	providers.push(
+		GoogleProvider({
+			clientId: GOOGLE_OAUTH_CLIENT_ID,
+			clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
+			profile: (profile) => {
+				return {
+					id: profile.sub,
+					name: profile.name,
+					firstName: profile.given_name,
+					lastName: profile.family_name,
+					email: profile.email,
+					image: profile.picture,
+				};
+			},
+		}),
 	);
 }
 
-if (!GITHUB_OAUTH_CLIENT_ID || !GITHUB_OAUTH_CLIENT_SECRET) {
-	throw new Error(
-		'GITHUB_OAUTH_CLIENT_ID and GITHUB_OAUTH_CLIENT_SECRET must be set',
+if (!!FACEBOOK_CLIENT_ID && !!FACEBOOK_CLIENT_SECRET) {
+	providers.push(
+		FacebookProvider({
+			clientId: FACEBOOK_CLIENT_ID,
+			clientSecret: FACEBOOK_CLIENT_SECRET,
+			profile: (profile) => {
+				return {
+					id: profile.id,
+					name: 'Juuso Lappalainen',
+					firstName: profile.name?.split(' ')[0] ?? '',
+					lastName: profile.name?.split(' ')[1] ?? '',
+					email: profile.email,
+					image: profile.picture?.data?.url,
+				};
+			},
+		}),
 	);
 }
 
-providers.push(
-	GoogleProvider({
-		clientId: GOOGLE_OAUTH_CLIENT_ID,
-		clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
-		profile: (profile) => {
-			return {
-				id: profile.sub,
-				name: profile.name,
-				firstName: profile.given_name,
-				lastName: profile.family_name,
-				email: profile.email,
-				image: profile.picture,
-			};
-		},
-	}),
-	GitHubProvider({
-		clientId: GITHUB_OAUTH_CLIENT_ID,
-		clientSecret: GITHUB_OAUTH_CLIENT_SECRET,
-		profile: (profile) => {
-			return {
-				id: profile.node_id,
-				name: profile.name,
-				firstName: profile.name?.split(' ')[0] ?? '',
-				lastName: profile.name?.split(' ')[1] ?? '',
-				email: profile.email,
-				image: profile.avatar_url,
-			};
-		},
-	}),
-);
+if (!!GITHUB_OAUTH_CLIENT_ID && !!GITHUB_OAUTH_CLIENT_SECRET) {
+	providers.push(
+		GitHubProvider({
+			clientId: GITHUB_OAUTH_CLIENT_ID,
+			clientSecret: GITHUB_OAUTH_CLIENT_SECRET,
+			profile: (profile) => {
+				return {
+					id: profile.node_id,
+					name: profile.name,
+					firstName: profile.name?.split(' ')[0] ?? '',
+					lastName: profile.name?.split(' ')[1] ?? '',
+					email: profile.email,
+					image: profile.avatar_url,
+				};
+			},
+		}),
+	);
+}
+
+if (providers.length === 0) {
+	throw new Error(
+		'Environment variables for at least one login provider must be configured.',
+	);
+}
 
 export const authOptions: NextAuthOptions = {
-	// Configure one or more authentication providers
 	providers,
 	adapter: PrismaAdapter(prisma),
 	theme: {
 		colorScheme: 'light',
-		brandColor: '#00ff00',
 	},
 	callbacks: {
 		session({ session, user }: any) {
