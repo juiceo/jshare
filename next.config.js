be1179cb575f50cd9053c2f030f7d7c92de4e7d4
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-26 12:18:53 +0200

    fix pwa config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/**
- * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
- */
-
-import nextPWA from 'next-pwa';
-
-const withPWA = nextPWA({
+/** eslint-disable */
+const withPWA = require('next-pwa')({
 	dest: 'public',
 	disabled: process.env.NODE_ENV === 'development',
 });
