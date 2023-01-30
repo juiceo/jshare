8a53edd525749fdf877539eb5b9f9016d557d789
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-30 12:44:34 +0200

    Disable sticky date separators

--- src/components/MessagesList/DateHeader.tsx
+++ src/components/MessagesList/DateHeader.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import { Button, Stack, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import moment from 'moment';
-import { useInView } from 'react-intersection-observer';
 
 interface DateHeaderProps {
 	date: Date;
 	messageId: string;
 }
-
-const MotionStack = motion(Stack);
 
 const DateHeader = (props: DateHeaderProps) => {
 	const { date } = props;
@@ -18,17 +15,14 @@ const DateHeader = (props: DateHeaderProps) => {
 	const formatDate = (date: Date) => {
 		const now = moment();
 		const dateMoment = moment(date);
-		const diff = now.diff(dateMoment, 'days');
-		if (diff === 0) {
+
+		if (now.isSame(date, 'day')) {
 			return 'Today';
 		}
-		if (diff === 1) {
-			return 'Yesterday';
-		}
-		if (diff < 7) {
+		if (now.isSame(date, 'week')) {
 			return dateMoment.format('dddd');
 		}
-		if (dateMoment.year() === now.year()) {
+		if (now.isSame(date, 'year')) {
 			return dateMoment.format('MMMM Do');
 		}
 		return dateMoment.format('MMMM Do, YYYY');
@@ -44,21 +38,13 @@ const DateHeader = (props: DateHeaderProps) => {
 	const formattedDate = formatDate(date);
 
 	return (
-		<MotionStack direction="column" alignItems="center" py="4" sx={{ position: 'sticky', top: '0' }}>
-			<Button
-				variant="outline"
-				colorScheme="gray"
-				borderRadius="2xl"
-				width="105px"
-				size="xs"
-				onClick={handleClick}
-				background="theme.pageBackground"
-			>
+		<Stack direction="column" alignItems="center" py="4">
+			<Button colorScheme="blackAlpha" borderRadius="2xl" px="4" size="xs" onClick={handleClick}>
 				<Text fontSize="xs" textAlign="center" py="1" margin="0">
 					{formattedDate}
 				</Text>
 			</Button>
-		</MotionStack>
+		</Stack>
 	);
 };
 
