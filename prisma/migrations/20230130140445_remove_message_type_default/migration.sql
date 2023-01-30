e41a9aefdfdf43722242d0e5f963b4f3e5fe75ed
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2023-01-30 16:15:38 +0200

    Remove messageType default in schema.prisma

---
+++ prisma/migrations/20230130140445_remove_message_type_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Message" ALTER COLUMN "type" DROP DEFAULT;
--- src/server/routers/messages.ts
+++ src/server/routers/messages.ts
@@ -12,83 +12,78 @@ import * as trpc from '@/server/trpc';
 const ee = new EventEmitter();
 
 export const messageRouter = trpc.router({
-	send: trpc.authenticatedProcedure
-		.input(createMessageSchema)
-		.mutation(async ({ input, ctx }) => {
-			try {
-				const group = await prisma.group.findUnique({
-					where: {
-						id: input.groupId,
-					},
-					include: {
-						members: true,
-						owner: true,
-					},
-				});
-
-				if (!group || !isUserInGroup(ctx.user.id, group)) {
-					throw new TRPCError({
-						code: 'NOT_FOUND',
-						message: 'Group not found',
-					});
-				}
-				const message = await prisma.message.create({
-					data: {
-						...input,
-						senderId: ctx.user.id,
-					},
-					include: {
-						sender: true,
-					},
-				});
-				ee.emit(Events.CreateMessageInGroup(input.groupId), message);
-				return message;
-			} catch (err) {
-				throw new TRPCError({
-					code: 'BAD_REQUEST',
-					message: 'Failed to send message',
-					cause: err,
-				});
-			}
-		}),
-	onSendMessageToGroup: trpc.authenticatedProcedure
-		.input(z.string())
-		.subscription(async ({ input }) => {
-			return observable<MessageWithSender>((emit) => {
-				const onSend = (data: MessageWithSender) => {
-					if (data.groupId === input) {
-						emit.next(data);
-					}
-				};
-				ee.on(Events.CreateMessageInGroup(input), onSend);
-
-				return () => {
-					ee.off(Events.CreateMessageInGroup(input), onSend);
-				};
+	send: trpc.authenticatedProcedure.input(createMessageSchema).mutation(async ({ input, ctx }) => {
+		try {
+			const group = await prisma.group.findUnique({
+				where: {
+					id: input.groupId,
+				},
+				include: {
+					members: true,
+					owner: true,
+				},
 			});
-		}),
-	listByGroupId: trpc.authenticatedProcedure
-		.input(z.string())
-		.query(async ({ input }) => {
-			try {
-				const messages = await prisma.message.findMany({
-					where: {
-						groupId: input,
-					},
-					include: {
-						sender: true,
-					},
-				});
 
-				return messages;
-			} catch (err) {
+			if (!group || !isUserInGroup(ctx.user.id, group)) {
 				throw new TRPCError({
-					code: 'BAD_REQUEST',
-					message: 'Failed to get messages',
-					cause: err,
+					code: 'NOT_FOUND',
+					message: 'Group not found',
 				});
 			}
-		}),
+			const message = await prisma.message.create({
+				data: {
+					...input,
+					senderId: ctx.user.id,
+					type: 'TEXT',
+				},
+				include: {
+					sender: true,
+				},
+			});
+			ee.emit(Events.CreateMessageInGroup(input.groupId), message);
+			return message;
+		} catch (err) {
+			throw new TRPCError({
+				code: 'BAD_REQUEST',
+				message: 'Failed to send message',
+				cause: err,
+			});
+		}
+	}),
+	onSendMessageToGroup: trpc.authenticatedProcedure.input(z.string()).subscription(async ({ input }) => {
+		return observable<MessageWithSender>((emit) => {
+			const onSend = (data: MessageWithSender) => {
+				if (data.groupId === input) {
+					emit.next(data);
+				}
+			};
+			ee.on(Events.CreateMessageInGroup(input), onSend);
+
+			return () => {
+				ee.off(Events.CreateMessageInGroup(input), onSend);
+			};
+		});
+	}),
+	listByGroupId: trpc.authenticatedProcedure.input(z.string()).query(async ({ input }) => {
+		try {
+			const messages = await prisma.message.findMany({
+				where: {
+					groupId: input,
+				},
+				include: {
+					sender: true,
+				},
+			});
+
+			return messages;
+		} catch (err) {
+			throw new TRPCError({
+				code: 'BAD_REQUEST',
+				message: 'Failed to get messages',
+				cause: err,
+			});
+		}
+	}),
 	listByGroupIdInfinite: trpc.authenticatedProcedure
 		.input(
 			z.object({
