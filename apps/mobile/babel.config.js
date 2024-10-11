316b40e34cbdbcf110f1886633a3159f32b1269a
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-10-11 10:48:07 -0700

    Add react-native-reanimated

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
+        plugins: [
+            /**
+             * react-native-reanimated plugin must be last
+             */
+            'react-native-reanimated/plugin',
+        ],
     };
 };
