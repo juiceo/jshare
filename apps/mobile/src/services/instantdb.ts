ddf9864cc48f4952871b2b6701a51044cdb41ade
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2024-10-21 17:38:45 -0700

    Import @jshare/db in apps/mobile

--- apps/mobile/src/services/instantdb.ts
+++ apps/mobile/src/services/instantdb.ts
@@ -1,5 +1,7 @@
-import { init } from '@instantdb/react-native';
+import { init_experimental } from '@instantdb/react-native';
+
+import schema from '@jshare/db/instant.schema';
 
 const APP_ID = 'ef6c50f7-d5d6-4c8d-a4ef-1dbc1f1cf7c4';
 
-export const db = init({ appId: APP_ID });
+export const db = init_experimental({ appId: APP_ID, schema });
