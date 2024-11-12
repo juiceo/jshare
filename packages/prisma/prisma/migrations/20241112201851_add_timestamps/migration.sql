e4d617afd3895d6dae57ef297e1b3934c4636cf7
Juuso Lappalainen <dev@juiceo.me>
2024-11-12 22:19:07 +0200

    Add timestamps to db models

---
+++ packages/prisma/prisma/migrations/20241112201851_add_timestamps/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "public"."group_participants" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."groups" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."profiles" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateIndex
+CREATE INDEX "group_participants_groupId_idx" ON "public"."group_participants"("groupId");
+
+-- CreateIndex
+CREATE INDEX "group_participants_userId_idx" ON "public"."group_participants"("userId");
