7aa758766ba70a99b65eadc532710d48b6f067a3
Juuso Lappalainen <dev@juiceo.me>
2025-01-05 18:11:36 +0200

    Add trpc endpoints for expenses

--- apps/server/src/trpc/router/expenses/index.ts
+++ apps/server/src/trpc/router/expenses/index.ts
@@ -1,11 +1,33 @@
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
+import { zCurrency, zLocalExpenseShare } from '@jshare/types';
+
 import { prisma } from '../../../services/prisma';
 import { authProcedure, router } from '../../trpc';
 
 export const expensesRouter = router({
-    listByGroup: authProcedure
+    get: authProcedure
+        .input(z.object({ id: z.string(), groupId: z.string() }))
+        .query(async (opts) => {
+            if (!opts.ctx.acl.isUserInGroup(opts.input.groupId)) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: `Group with id ${opts.input.groupId} not found`,
+                });
+            }
+
+            return prisma.expense.findUnique({
+                where: {
+                    id: opts.input.id,
+                    groupId: opts.input.groupId,
+                },
+                include: {
+                    shares: true,
+                },
+            });
+        }),
+    list: authProcedure
         .input(
             z.object({
                 groupId: z.string(),
@@ -38,4 +60,107 @@ export const expensesRouter = router({
                 },
             });
         }),
+    create: authProcedure
+        .input(
+            z.object({
+                groupId: z.string(),
+                payerId: z.string(),
+                amount: z.number().min(1),
+                currency: zCurrency,
+                shares: zLocalExpenseShare.array().min(1),
+            })
+        )
+        .mutation(async (opts) => {
+            if (!opts.ctx.acl.isUserInGroup(opts.input.groupId)) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: `Group with id ${opts.input.groupId} not found`,
+                });
+            }
+            const totalAmountFromShares = opts.input.shares.reduce((result, share) => {
+                if (share.amount) return result + share.amount;
+                return result;
+            }, 0);
+
+            if (totalAmountFromShares !== opts.input.amount) {
+                throw new TRPCError({
+                    code: 'BAD_REQUEST',
+                    message: `Total amount from shares (${totalAmountFromShares}) does not match the amount (${opts.input.amount})`,
+                });
+            }
+
+            const expense = await prisma.expense.create({
+                data: {
+                    ownerId: opts.ctx.userId,
+                    groupId: opts.input.groupId,
+                    payerId: opts.input.payerId,
+                    amount: opts.input.amount,
+                    currency: opts.input.currency,
+                    shares: {
+                        createMany: {
+                            data: opts.input.shares.map((share) => ({
+                                userId: share.userId,
+                                amount: share.amount,
+                                currency: opts.input.currency,
+                                enabled: share.enabled,
+                                locked: share.locked,
+                            })),
+                        },
+                    },
+                },
+            });
+
+            return expense;
+        }),
+    totalByGroup: authProcedure.input(z.object({ groupId: z.string() })).query(async (opts) => {
+        if (!opts.ctx.acl.isUserInGroup(opts.input.groupId)) {
+            throw new TRPCError({
+                code: 'NOT_FOUND',
+                message: `Group with id ${opts.input.groupId} not found`,
+            });
+        }
+
+        const result = await prisma.expense.aggregate({
+            _sum: {
+                amount: true,
+            },
+            where: {
+                groupId: opts.input.groupId,
+            },
+        });
+
+        return result._sum.amount ?? 0;
+    }),
+    getTotalBalance: authProcedure.query(async (opts) => {
+        const userId = opts.ctx.userId;
+
+        const [paid, received] = await Promise.all([
+            prisma.expense
+                .aggregate({
+                    _sum: {
+                        amount: true,
+                    },
+                    where: {
+                        payerId: userId,
+                    },
+                })
+                .then((res) => res._sum.amount ?? 0),
+            prisma.expenseShare
+                .aggregate({
+                    _sum: {
+                        amount: true,
+                    },
+                    where: {
+                        userId,
+                    },
+                })
+                .then((res) => res._sum.amount ?? 0),
+        ]);
+
+        return {
+            paid,
+            received,
+            balance: paid - received,
+        };
+    }),
 });
