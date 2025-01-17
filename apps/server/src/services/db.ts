c2d94baf62197d6e9f510c457d7b480b0b79582c
Juuso Lappalainen <dev@juiceo.me>
2025-01-17 19:13:57 +0200

    Add superuser db client

--- apps/server/src/services/db.ts
+++ apps/server/src/services/db.ts
@@ -1,4 +1,20 @@
+import type { DB } from '@jshare/db';
 import { enhance, PrismaClient } from '@jshare/db/server';
 
 const prisma = new PrismaClient();
-export const db = enhance(prisma);
+
+/**
+ * "Superuser" database client, bypasses all ACL checks
+ */
+export const db = enhance(
+    prisma,
+    {},
+    { kinds: ['validation', 'delegate', 'password', 'encryption'] }
+);
+
+/**
+ * Get a database client that enforces ACL checks for a given user
+ */
+export const getDb = (user: DB.Profile) => {
+    return enhance(prisma, { user });
+};
