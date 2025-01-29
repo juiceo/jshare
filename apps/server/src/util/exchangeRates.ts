bf0812700ed3372676189b82ed89202c4683923f
Juuso Lappalainen <dev@juiceo.me>
2025-01-29 22:28:48 +0200

    Improve exchange rates logic

--- apps/server/src/util/exchangeRates.ts
+++ apps/server/src/util/exchangeRates.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { z } from 'zod';
 
 import { BASE_EXCHANGE_RATES } from '@jshare/common';
@@ -9,48 +10,48 @@ export const getLatestExchangeRates = async (): Promise<DB.ExchangeRates> => {
     /**
      * TODO: Caching for the database call - no need to always query the database since this data never changes
      */
-    const dbRates = await db.exchangeRates
+    const latestRates = await db.exchangeRates
         .findFirst({
-            where: {
-                createdAt: {
-                    gte: new Date(Date.now() - 1000 * 60 * 60 * 24),
-                },
-            },
             orderBy: {
                 createdAt: 'desc',
             },
         })
         .then((res) => res as DB.ExchangeRates | null);
 
-    if (dbRates) return dbRates;
+    if (latestRates && dayjs(latestRates.createdAt).isAfter(dayjs().subtract(1, 'day')))
+        return latestRates;
 
-    const latestRates = await fetchExchangeRates();
-    const rates = await db.exchangeRates.create({
-        data: {
-            baseCurrency: 'USD',
-            rates: {
-                ...BASE_EXCHANGE_RATES.rates,
-                ...latestRates,
+    const newRates = await fetchExchangeRates();
+    return db.exchangeRates
+        .create({
+            data: {
+                baseCurrency: 'USD',
+                rates: {
+                    ...BASE_EXCHANGE_RATES.rates,
+                    ...latestRates?.rates,
+                    ...newRates,
+                },
             },
-        },
-    });
-
-    return rates as DB.ExchangeRates;
+        })
+        .then((res) => res as DB.ExchangeRates);
 };
 
 const fetchExchangeRates = async (): Promise<Partial<DB.ExchangeRatesObject>> => {
-    console.log('FETCHING EXCHANGE RATES...');
-
     const API_KEY = process.env.CURRENCY_FREAKS_API_KEY;
     if (!API_KEY) {
         console.warn("Currency Freaks API key is missing. Exchange rates won't be updated.");
         return {};
     }
-    const rates = await fetch(
-        `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}&base=USD&symbols=${Object.values(DB.CurrencyCode).join(',')}`
-    );
+    try {
+        const rates = await fetch(
+            `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${API_KEY}&base=USD&symbols=${Object.values(DB.CurrencyCode).join(',')}`
+        );
 
-    const data = await rates.json();
+        const data = await rates.json();
 
-    return z.record(zDB.enums.CurrencyCodeSchema, z.coerce.number()).parse(data.rates);
+        return z.record(zDB.enums.CurrencyCodeSchema, z.coerce.number()).parse(data.rates);
+    } catch (err: any) {
+        console.error('Error fetching exchange rates: ' + err.message);
+        return {};
+    }
 };
