5bed315022819f2e656f268693047dfec43b973b
Juuso Lappalainen <dev@juiceo.me>
2025-01-31 01:19:42 +0200

    Add server endpoint for temp users

--- apps/server/src/trpc/router/groupParticipants/index.ts
+++ apps/server/src/trpc/router/groupParticipants/index.ts
@@ -1,5 +1,8 @@
 import { TRPCError } from '@trpc/server';
+import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod';
+
+import { DB } from '@jshare/db';
 
 import { db } from '../../../services/db';
 import { authProcedure, router } from '../../trpc';
@@ -28,4 +31,39 @@ export const groupParticipantsRouter = router({
             },
         });
     }),
+    createTemporaryParticipant: authProcedure
+        .input(
+            z.object({
+                groupId: z.string(),
+                firstName: z.string(),
+                lastName: z.string(),
+            })
+        )
+        .mutation(async (opts) => {
+            const isUserInGroup = await opts.ctx.acl.isInGroup(opts.input.groupId);
+            if (!isUserInGroup) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: `Group with id ${opts.input.groupId} not found`,
+                });
+            }
+
+            const user = await db.profile.create({
+                data: {
+                    email: '',
+                    userId: uuidv4(),
+                    firstName: opts.input.firstName,
+                    lastName: opts.input.lastName,
+                    currency: DB.CurrencyCode.USD,
+                },
+            });
+
+            return db.groupParticipant.create({
+                data: {
+                    groupId: opts.input.groupId,
+                    role: DB.Role.Member,
+                    userId: user.userId,
+                },
+            });
+        }),
 });
