c6cedfb240b4613c4248cf1778fb29031e0bd557
Juuso Lappalainen <dev@juiceo.me>
2025-05-11 11:56:07 +0300

    Fix pug template

--- apps/server/src/static/deeplinks/index.ts
+++ apps/server/src/static/deeplinks/index.ts
@@ -1,10 +1,14 @@
 import path from 'path';
 import { Router } from 'express';
+import pug from 'pug';
 
 export const router = Router();
 
 router.get('/invite/:code', (req, res) => {
-    res.sendFile(path.join(__dirname, 'invite.html'));
+    const templatePath = path.resolve(__dirname, 'invite.pug');
+    const html = pug.renderFile(templatePath, { groupName: 'A group name here' });
+
+    res.send(html);
 });
 
 export default router;
