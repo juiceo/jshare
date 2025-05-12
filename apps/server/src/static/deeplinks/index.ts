25571df15d50bd86683156e3612c80328e78a0be
Juuso Lappalainen <dev@juiceo.me>
2025-05-12 15:33:55 +0300

    Add App Store link to invite page

--- apps/server/src/static/deeplinks/index.ts
+++ apps/server/src/static/deeplinks/index.ts
@@ -1,11 +1,15 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 import { db } from '../../services/db';
-import { supabase } from '../../services/supabase';
 
 export const router = Router();
 
-router.get('/invite/:code', async (req, res) => {
+const isIOS = (req: Request) => {
+    const userAgent = (req.headers as any)['user-agent'];
+    return /iPhone|iPad|iPod|Macintosh/.test(userAgent);
+};
+
+router.get('/invite/:code', async (req: Request, res: Response) => {
     const group = await db.group.findUnique({
         where: {
             inviteCode: req.params.code,
@@ -15,6 +19,8 @@ router.get('/invite/:code', async (req, res) => {
             participants: true,
         },
     });
+
+    console.log('REQ', req.headers);
 
     if (!group) {
         res.render('invite-invalid', {
@@ -29,6 +35,12 @@ router.get('/invite/:code', async (req, res) => {
         res.render('invite', {
             groupName: group.name,
             inviteCode: group.inviteCode,
+            appStoreUrl: isIOS(req)
+                ? 'https://apps.apple.com/fi/app/jshare/id6743673035'
+                : /**
+                   * TODO: Add correct Google Play URL once we have it
+                   */
+                  'https://play.google.com/store/apps/details?id=com.jshare.app',
             meta: {
                 title: `Join ${group.name} on JShare`,
                 description: `Click to accept the invite and start collaborating in ${group.name}.`,
